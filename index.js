b83dee92b86ec56a8db6ae39c91b24ac9eff0003
vishnuvardhan7794 <59390180+vishnuvardhan7794@users.noreply.github.com>
2023-02-11 21:14:18 +0530

    Programs

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-// Import stylesheets
-import './style.css';
+// Palindrom /[\W_]/g or /[^A-Za-z0-9]/g
+function palindrome(word) {
+  let re = /[^A-Za-z0-9]/g;
+  const newWord = word.toLowerCase().replace(re, '');
+  const reversWord = newWord.split('').reverse().join('');
+  return newWord === reversWord;
+}
 
-// Write Javascript code!
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+console.log('isPalindrom', palindrome('dfsd'));
+
+// title Case
+function titleCase(sentence) {
+  const words = sentence.split(' ');
+  const newFormat = [];
+  for (let i = 0; i < words.length; i++) {
+    let firstLatter = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    newFormat.push(firstLatter);
+    console.log('firstLatter', firstLatter, words[i].slice(1));
+  }
+  console.log('newFormat', ...newFormat);
+}
+
+// titleCase("I'm a little tea pot");
+
+// logenst word in sentence
+var strSplit = 'The quick brown fox jumped over the lazy dog'.split(' ');
+let longestWordIndex = 0;
+let wordLength = 0;
+for (let i = 0; i < strSplit.length; i++) {
+  if (wordLength < strSplit[i].length) {
+    wordLength = strSplit[i].length;
+    longestWordIndex = i;
+  }
+}
